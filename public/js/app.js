d35e780d772601cc1aaa352523e56e098744374c
sebaabr <88507698+sebaabr@users.noreply.github.com>
2021-11-25 08:52:06 +0100

    Setup app for Heoku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) =>{
         return messageTwo.textContent = 'Provide the location';
     } 
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then(res => res.json())
     .then(data => {
         if(data.error) {
